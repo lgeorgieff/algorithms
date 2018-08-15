bb82d7a6702d0a5a190266893f5dfc83ac95794b
Lukas Georgieff <lukas.georgieff@hotmail.com>
2018-08-14 20:09:11 -0700

    src: Add iterative DFS search for graphs

--- src/graphs/dfs.cpp
+++ src/graphs/dfs.cpp
@@ -19,7 +19,9 @@ void dfs_recursive_p(std::stack<std::shared_ptr<algorithms::graph::node<T>>> &cu
 
   fn(current_nodes.top()->value);
   visited.insert(current_nodes.top());
-  for(auto child : current_nodes.top()->connections()) {
+  auto current{current_nodes.top()};
+  current_nodes.pop();
+  for(auto child : current->connections()) {
     auto sptr{child.lock()};
     if(sptr != nullptr) current_nodes.push(sptr);
   }
@@ -31,7 +33,26 @@ void dfs_recursive_p(std::stack<std::shared_ptr<algorithms::graph::node<T>>> &cu
 
 template<typename T>
 void algorithms::graph::dfs_iterative(std::shared_ptr<node<T>> current_node, std::function<void(const T &)> fn) {
+  algorithms::graph::unordered_set_shared_node<T> visited;
+  std::stack<std::shared_ptr<node<T>>> storage;
+  storage.push(current_node);
 
+  while(!storage.empty()) {
+    if(visited.find(storage.top()) != visited.cend()) {
+      storage.pop();
+      continue;
+    }
+
+    visited.insert(storage.top());
+    fn(storage.top()->value);
+    auto current{storage.top()};
+    storage.pop();
+
+    for(auto child : current->connections()) {
+      auto sptr{child.lock()};
+      if(sptr != nullptr) storage.push(sptr);
+    }
+  }
 }
 
 template<typename T>
